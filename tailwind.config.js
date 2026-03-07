af80cc9fe69195ede5ac3b4f7c8df7550aa19205
JeanLTX <jeanluccaltx@gmail.com>
2026-03-07 00:14:05 -0300

    teste aumento de performance

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./script.js"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#6c5ce7',
+                'primary-dark': '#5d4aec',
+                dark: '#1e1e1e',
+                light: '#f8f9fa',
+                'deep-dark': '#0C0B19',
+            }
+        },
+    },
+    plugins: [],
+    corePlugins: {
+        preflight: false, // ISSO impede que o Tailwind resete seus estilos existentes
+    }
+}
